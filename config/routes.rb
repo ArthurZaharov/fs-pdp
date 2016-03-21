9351538473219dccc535994d5715c270d0e27e54
ArthurZaharov <arthur.zaharov@flatstack.com>
2016-03-21 15:53:36 +0300

    Add feedback form

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
     resources :comments, only: :create
   end
 
+  resource :feedback, only: %i(new create)
+
   get "*page", to: "static_pages#show", constraints: { page: :about_us }
 end

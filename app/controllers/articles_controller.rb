9589539af0c1a79e902d20ac251e4ff5495ae0ae
ArthurZaharov <arthur.zaharov@flatstack.com>
2016-03-30 18:24:22 +0300

    Cache view with article and article's user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   before_action :authenticate_user!, only: %i(new create edit update)
   before_action :authorize_user!, only: %i(edit update)
 
-  expose_decorated(:articles) { |scope| scope.recent.limit(10) }
+  expose_decorated(:articles) { |scope| scope.includes(:user).recent.limit(10) }
   expose_decorated(:article, attributes: :article_params)
   expose(:comment) { article.comments.build }
   expose_decorated(:comments) { article.comments.includes(:user) }

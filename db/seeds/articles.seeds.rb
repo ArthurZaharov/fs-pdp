0a39246b71a2eec41f78ae77e7ee78fecef75fae
ArthurZaharov <arthur.zaharov@flatstack.com>
2016-04-04 19:42:22 +0300

    Add more seeds

--- db/seeds/articles.seeds.rb
+++ db/seeds/articles.seeds.rb
@@ -18,13 +18,101 @@ articles = [
       In January, MEL Films spent a few days at High Chaparral, chronicling the displaced Syrians who are living among Swedes who are imitating 19th century American cowboys.
       Watch our full 9-minute documentary above; also be sure to check out our slideshow of life at High Chaparral.
     CONTENT
+  },
+  {
+    title: "Finding purpose in the loss of our son",
+    content: <<-CONTENT.strip_heredoc
+      Disclaimer: This post is primarily a selfish endeavor. It is a channel for my grief. More importantly, I hope the words about our experience and our healing process will provide comfort and solace to the next set of parents that have to go through the unimaginable loss of their child. You are not alone.
+      It was the morning after all the chaos from my wife’s sudden and unforeseen preterm labor. I was standing in the NICU staring down at my prematurely born son. His eyes were closed as his chest rose and fell with each machine-aided breath. My eyes were full of hope and fear as I looked down on this small human that I was meant to protect and nurture— but I couldn’t because instead of being in my arms or in his mother’s, he was inside a NICU incubator with an unfathomable number of tubes attached to him. At 25-weeks gestational age, he was not supposed to be here yet.
+      Reality hadn’t quite set in yet. The trauma of the previous night’s events hadn’t lifted. My wife and I were scared. The doctors told us what we already knew — our son was incredibly premature, his lungs, heart, and skin were not fully developed — the situation was not good. So much had happened and it all seemed surreal. 24 hours prior, everything was fine. Then, it was anything but fine. The shock suffocated me.
+    CONTENT
+  },
+  {
+    title: "Buy All Your Furniture at Target, For Tomorrow We Die",
+    content: <<-CONTENT.strip_heredoc
+      his time last year, I didn’t own a nightstand. I didn’t own a mattress or a single chair. I’d like to say this is because I’m like Marie Kondo-meets-Braveheart, yelling “FREEDOM!” in the face of stuff, but the truth is more complicated. And more typical, too, I suspect.
+      Here’s the story. Since my husband and I got together in 2006, we’ve moved more than a dozen times. This includes the time we shifted three blocks, and the two times we moved abroad — to New Zealand, and later, Australia — or about as far as it’s possible to go before you start circumnavigating.
+      Which is how you get to be 33 and own only a couple of boxes of books and novelty coffee mugs. I could never say which was the chicken and which the egg; we didn’t own much because we’d moved so many times, while not owning much was what allowed us to keep moving.
+      When, in March of 2015, we finally bought a place — a modestly priced, 1,500 square foot house in our hometown that we planned to use as a permanent crash pad — we had to furnish it from scratch. Either that, or we’d be camping inside our own house forever.
+      Furniture can be eye-wateringly expensive. But the expense wasn’t even the most daunting thing. I dreaded spending Saturday mornings examining upholstery under florescent lights. I couldn’t face all the tedious deciding, or the depressing comfortable footwear (furniture stores have the hardest floors in the world, a millimeter of carpet over three feet of concrete) or the desperately chipper salespeople treating me like a real adult, like my own mother. I’d rather stay in my sleeping bag.
+    CONTENT
+  },
+  {
+    title: "My 10 Favorite Wrestlemania Matches",
+    content: <<-CONTENT.strip_heredoc
+      I think this is my very favorite Wrestlemania match ever.
+      It is an “I Quit” match, which means it can only end when one of the wrestlers submits and says those words. Bret and Austin had a violent blood feud going into this, and that fury carries itself into the match. They fight into the bleachers, up the stairs and back down, destroying each other the whole way. They get back into the ring, and though they don’t put on a very scientific wrestling match, they engage in such physical and psychological warfare that the crowd is whipped into a frenzy, more so than they could have ever expected.
+      At a certain point, Austin is thrown into the barricade and his head his busted open. Secretly, Austin had cut himself with a hidden razorblade. Though this had been common practice in pro wrestling for decades, wrestlers in WWE had been told to stop this in order to clean up the product for the kids. But before the match, Bret told Austin that the only way the finish of their match would be believable would be if Austin bled. Like a true professional, Austin concurred.
+      After the two men exchange strangulations and chair shots and ring bell shots, Bret eventually gets his dreaded Sharpshooter submission hold on Austin. Against his better judgment, Austin refuses to say “I Quit.” Instead he stays in the hold and fights and screams and bleeds buckets, until he passes out from the pain. Bret wins, but his actions after the match make him repellant to the crowd.
+    CONTENT
+  },
+  {
+    title: "I Have Met the Bernie Bro (and He is Us).",
+    content: <<-CONTENT.strip_heredoc
+      There we were at our Washington state caucus, engaging in the democratic process. I was thrilled to see the large turnout of young voters, and eager to hear their reasons for engaging with the body politic.
+      A very young man fumbled through a statement about the exorbitant price of college. He veered into a criticism of the Clinton administration and the implied trustworthiness of the Clinton brand. We older voters patiently waited for the history lesson to end.
+      And then it happened: he blamed Hillary Clinton for her husband’s sexist behavior in office. His exact words disappeared beneath a white roaring in my ears. Women flinched. Several men winced. Three of us begged audibly, “Stay positive!”
+      But the damage was done: an undecided Clinton-leaning woman near me froze. Her face hardened into decision, and I empathized.
+      My Clinton-leaning friends are nodding. You’ve been telling me that shrill, tone-deaf and outright sexist Sanders supporters are the biggest problem with my candidate for months. You insist we’re positively uncivil and it makes it hard to #feelthebern.
+      Just before our young man spoke, the voter standing behind him took her turn. An older woman with a defensive posture, she declaimed in a brittle, imperious voice just how unfair it would be to force her to help freeloading kids through college. She spoke of putting herself through college, uphill both ways, carving her pencils from raw graphite. Her husband glowered.
+      She wrapped up by gazing around the packed precinct and accusing Bernie supporters of ruining democracy. She predicted that we political dilettantes wouldn’t stick around. As soon as our precinct went 5:1 for Sanders she stomped out, husband silently in tow, not stopping to help select our sole Clinton delegate.
+      My Sanders-leaning friends are nodding. We’ve encountered entitled Clinton supporters who lecture us on apathy and call us freeloaders while railing against our citizen-funded, people-powered campaign.
+      Our older caucus-goer’s rant about the entitlement of youth hit a nerve in our young man. When he saw hard judgement written on her face, he reacted with defensive anger. Like a teetering domino, his anger then toppled the compassion of the voter beside me.
+    CONTENT
+  },
+  {
+    title: "Farewell to Jack English, Who Built a Life in an American Wilderness",
+    content: <<-CONTENT.strip_heredoc
+      In September 1976, Jack and Mary English were hunting with their 14 year-old son Dennis in the woods behind Big Sur, California. A 260-square-mile section of national forest with rugged peaks, hidden valleys, and hot springs, the Ventana Wilderness is located in a region known for having California’s largest density of mountain lion, and abundant wild hogs, turkeys and deer. While Jack and Dennis were off tracking a deer, Mary encountered a small group of 20-something hikers who were holding a map and looking around curiously. They said they’d read a classified ad in the local newspaper: someone was auctioning off a 5-acre plot somewhere in the middle of this national forest in a place called Pine Valley. The hikers had found the right spot. Later, after they’d left and Jack and Dennis returned, Mary relayed the news. “Somebody is gonna get this land,” she said, “It’s gonna be us.”
+      Since 1930, when Jack was eleven, he had been frequenting Pine Valley to hike, camp, hunt and fish for rainbow trout. His family lived on a farm about 50 miles north, as the crow flies. Surrounded by a forest of ponderosa pine, Pine Valley is only accessible on foot or on horseback via a pair of dusty trails that descend and meander 6 miles through the rocky Santa Lucia Mountains. Around 1880, after the passage of the Homestead Act, settlers began staking claims on 160 acres in and around Pine Valley. Over the years, families continued trading back their undeveloped parcels to the Forest Service. Jack had come to know one 15-acre plot well. It was situated right along a stream, near a sunny pasture, below a massive sandstone formation that glows in the moonlight. There were dilapidated remains of an old cabin, but no one had lived there in years. So in 1936, when Jack was seventeen, he contacted the owner. She wouldn’t take less than $1,000 per acre (For all 15 acres, that translates to roughly $257,000 today). Oh, well, Jack figured.
+    CONTENT
+  },
+  {
+    title: "A behind-the-scenes look at March on the campaign trail with Hillary Clinton",
+    content: <<-CONTENT.strip_heredoc
+      March was the busiest month of the 2016 campaign so far: Democrats held 28 contests in 31 days and awarded almost half of the delegates up for grabs in the Democratic primary.
+      Hillary Clinton and President Clinton spent the whirlwind month traveling the country — hitting 18 states and fighting for every single vote.
+      Here’s a look back at a few great moments along the way.
+    CONTENT
+  },
+  {
+    title: "Museum Logos: Drawing The Line",
+    content: <<-CONTENT.strip_heredoc
+      The change got me thinking about Museum logos more generally, and I started spending more time analysing them. (Because that’s just how fun I am). While looking over a few visual identities, I stumbled across a trend. A harrowing commonality that suggests some international conspiracy between our cultural gatekeepers. I am equal parts scared and excited to present my findings to you today.
+      What are we looking at here? Sure, it’s the name of the museum, but what’s important is that it’s an abbreviation where the two letters get smushed together along a diagonal line. It’s a simple way of depicting the different disciplines of the Museum, while simultaneously symbolising the influence of both Victoria *and* Albert.
+      I thought I’d noticed this quirk somewhere else, and then I recognised it in the logo of my own workplace, the Museum of Applied Arts and Sciences:
+      Not only do the diagonal lines touch, but they’re overlapping! And if you look closely (which clearly I have), there are even diagonal lines in the background!
+      At this point I started to panic. I needed more data. Maybe this was just an Australian thing. Let’s pick a country at random. Poland? You got it. Here’s the National Museum of Warsaw
+    CONTENT
+  },
+  {
+    title: "A Hacker’s Guide to Bending the Universe",
+    content: <<-CONTENT.strip_heredoc
+      hen it’s functioning properly, a cathode ray tube is an engineering marvel more impressive than the Golden Gate Bridge.
+      You might have seen a tube in an older TV, but to appreciate it fully is to know what’s happening within. The inside of a cathode ray tube is a near vacuum, a close cousin to outer space. The glass holding it together, shaped like a curious chalice, is strong enough to protect both the entire apparatus from imploding, and viewers from being bombarded with too much radiation. Yes, radiation: it employs a small gun that shoots particles at a very high speed, surrounded by a set of coils controlling their direction. On the other side, a precisely machined mesh gets those particles re-aligned — like a shrunken-down game of skee ball — just before they reach their final destination, coated in phosphor.
+      And then there are the particles themselves. Discovered as “cathode rays,” they were later renamed “electrolions,” and eventually “electrons.” When the tube is on, they travel by the tens of millions any given second, following a complicated pattern that zigzags from top-left to bottom-right, timed perfectly so that the phosphor coating is hit at the right place and time. The kind of phosphor is chosen precisely, too, staying lit for just the right amount of time until it is bombarded again, and again, and again.
+      But a human face on the other side of the vacuum, and the gun, and the phosphor, will see none of their precise choreography, and might not even be aware of their existence.
+      What it will see, instead, is the moving image.
+    CONTENT
+  },
+  {
+    title: "Cuttin’ for your country",
+    content: <<-CONTENT.strip_heredoc
+      Woodchopping used to be must-watch television — like Top Town, Mastermind and It’s In The Bag. But while the sport has lost its profile, its never quite gone away. In late summer, Peter Malcouronne went to the small Bay of Plenty town of Katikati to watch a down-on-its-luck New Zealand side take on Australia.
+      Head past the haunted house and the bouncy castle, past the ride-on lawnmowers and around the racing pigs (reputedly the fastest in all Katikati) and you’ll see them — 10 giants in green tracksuits, driven by drizzle to a bloke’s huddle under a cypress tree. “So this is the New Zealand summer?” says Justin Beckett drolly.
+      He’s one of the star draws for the Katikati Axemen’s and Community Carnival, held alongside the town’s annual A&P show. An insouciant fellow with thick, greying hair and stunted goatee, Beckett’s a big man. No — he’s a huge man: 6ft 4in (193cm), nearly 22 stone (139kg), with a Minotaur’s shoulders and hands like coal buckets. He’s been cutting for his country for 11 years and now, at 31, is reaching his prime as an axeman. He’s won several world titles, but his finest moment could be just four hours away when he’ll lead Australia against New Zealand in the first of three woodchopping tests.
+      Hoping to get a decisive edge for his team, Beckett will chop first in the six-event relay. He’ll cut the underhand, standing astride a block and chopping it between his legs, then a team-mate will take over on the standing block, so named because you simply stand beside the block and hack it in half. The sawyers — the single first, then the two-man double — follow, then a second underhand and the final standing block.
+      All going to plan, Beckett’s contribution will take no more than 30 seconds — the time it takes him to blast through a 325mm diameter block. His team should finish inside three minutes. All going to plan.
+      But as Beckett stalks the Katikati woodchopping arena, he admits there’s a lot that can go wrong. There’s the wood for a start. The Australians are used to cutting hardwoods — gum, ash, jarrah — but the wood here at Katikati is Lombardy poplar for the warm­ up events, Monterey pine — pinus radiata — for the tests. Softer wood but not always easier.
+      “Radiata’s a nice wood to chop if it’s good radiata,” Beckett drawls. “If it’s grown in a plantation quick, then it’s real soft. But old radiata has very little sap — it’s full of cork. I’ve had some shocking experiences with the stuff. You hit it as hard as you like and the axe doesn’t go in. It’s like hitting a lump of dry, old leather. It’s not so bad when you’re cuttin’ for yourself but when you’re cuttin’ for your team, your country — when there’s half a dozen Aussie fellas relyin’ on ya — it’s a real kick in the teeth.”
+    CONTENT
   }
 ]
 
 after(:users) do
   articles.each do |attrs|
     article = Article.find_or_initialize_by(title: attrs[:title])
-    article.user = User.find_by_email("author@example.com")
+    article.user = User.all.sample
     article.attributes = attrs
     article.save!
   end
--- db/seeds/users.seeds.rb
+++ db/seeds/users.seeds.rb
@@ -3,14 +3,56 @@ users = [
     email: "user@example.com",
     password: "123456",
     full_name: "John Doe",
-    city: "New Orleans",
+    city: "New York",
+    address: "291-343 Hops Ln",
     confirmed_at: 1.hour.ago
   },
   {
     email: "author@example.com",
     password: "123456",
+    full_name: "Arthur Zaharov",
+    city: "Kazan",
+    address: "Pushkina, 46",
+    confirmed_at: 1.hour.ago
+  },
+  {
+    email: "author1@example.com",
+    password: "123456",
     full_name: "Marco Polo",
     city: "New York",
+    address: "1680 Coney Island Ave",
+    confirmed_at: 1.hour.ago
+  },
+  {
+    email: "author2@example.com",
+    password: "123456",
+    full_name: "Jonny Depp",
+    city: "New York",
+    address: "23 E 3rd St",
+    confirmed_at: 1.hour.ago
+  },
+  {
+    email: "author3@example.com",
+    password: "123456",
+    full_name: "Angelina Jolly",
+    city: "New Orleans",
+    address: "11417 Campbell Ln",
+    confirmed_at: 1.hour.ago
+  },
+  {
+    email: "author4@example.com",
+    password: "123456",
+    full_name: "Teddy the Pohh",
+    city: "New Orleans",
+    address: "3704 Arkansas Ave",
+    confirmed_at: 1.hour.ago
+  },
+  {
+    email: "author5@example.com",
+    password: "123456",
+    full_name: "Tod Island",
+    city: "New Orleans",
+    address: "1128 Tensas Dr",
     confirmed_at: 1.hour.ago
   }
 ]

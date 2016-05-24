dc4be62a78e35e3d0f16ece860f646403177b6ab
ArthurZaharov <arthur.zaharov@flatstack.com>
2016-05-25 00:41:56 +0300

    Find authors by they ids

--- app/controllers/search_authors_controller.rb
+++ app/controllers/search_authors_controller.rb
@@ -2,7 +2,7 @@ class SearchAuthorsController < ApplicationController
   respond_to :json
 
   expose(:articles) { Article.search_by_content(params[:query]) }
-  expose(:authors) { User.where(id: articles.pluck(:id)).limit(5) }
+  expose(:authors) { User.where(id: articles.pluck(:user_id)).limit(5) }
 
   def index
     respond_with(authors)

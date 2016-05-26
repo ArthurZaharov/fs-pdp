aefd67067624c0f6b918f7785a7f64fc875e37fd
ArthurZaharov <arthur.zaharov@flatstack.com>
2016-05-27 00:24:09 +0300

    Add subscription prices to edit profile page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160525200441) do
+ActiveRecord::Schema.define(version: 20160526202021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20160525200441) do
     t.string   "address"
     t.float    "latitude"
     t.float    "longitude"
+    t.jsonb    "subscription_prices"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree

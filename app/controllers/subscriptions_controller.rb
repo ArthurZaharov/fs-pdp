2836f47bd40739f0bd202600c621b659d7328ce1
ArthurZaharov <arthur.zaharov@flatstack.com>
2016-05-31 11:44:44 +0300

    Doesn't render subscription prices for guest

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -4,20 +4,24 @@ class SubscriptionsController < ApplicationController
   expose_decorated(:author, model: :user, decorator: UserDecorator)
 
   def create
-    result = SubscribeToAuthor.call(
+    if subcribe_to_author.success?
+      flash[:notice] = "Subscription created"
+    else
+      flash[:alert] = subcribe_to_author.message
+    end
+
+    redirect_to(root_path)
+  end
+
+  private
+
+  def subcribe_to_author
+    @subcribe_to_author ||= SubscribeToAuthor.call(
       subscription_type: params[:subscription_type],
       author_id: params[:author_id],
       current_user_id: current_user.id,
       stripe_email: params[:stripeEmail],
       stripe_token: params[:stripeToken]
     )
-
-    if result.success?
-      flash[:notice] = "Subscription created"
-    else
-      flash[:alert] = result.message
-    end
-
-    redirect_to(root_path)
   end
 end

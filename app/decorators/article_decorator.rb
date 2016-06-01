28417d38e108b70037987a917777eca3b3df0f56
ArthurZaharov <arthur.zaharov@flatstack.com>
2016-06-01 19:37:40 +0300

    Add free/paid labels

--- app/decorators/article_decorator.rb
+++ app/decorators/article_decorator.rb
@@ -1,3 +1,13 @@
 class ArticleDecorator < ApplicationDecorator
   delegate :id, :title, :kind, :content, :user
+
+  def type
+    content_tag(:span, "#{object.kind} article", class: "label radius #{label_color}")
+  end
+
+  private
+
+  def label_color
+    object.paid? ? "" : "secondary"
+  end
 end

67d2127c403b2b76a7bf72787ebda1a671d78635
ArthurZaharov <arthur.zaharov@flatstack.com>
2016-06-02 13:37:13 +0300

    Update articles to which user subscribed for caching

--- db/seeds/subscriptions.seeds.rb
+++ db/seeds/subscriptions.seeds.rb
@@ -1,12 +1,10 @@
 after(:users) do
   user = User.find_by_email("user@example.com")
 
-  3.times do
-    user
-      .subscriptions
-      .create!(
-        author: User.where.not(id: user.id).sample,
-        expired_at: Time.zone.today + 30.days
-      )
-  end
+  user
+    .subscriptions
+    .create!(
+      author: User.where.not(id: user.id).sample,
+      expired_at: Time.zone.today + 30.days
+    )
 end
